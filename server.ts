4f28d1da906231efacfbe186b915a66adc6c6841
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-21 12:35:50 +0330

    feat: connected to mongodb

--- server.ts
+++ server.ts
@@ -3,12 +3,10 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-console.log(process.env);
-
 mongoose
   .connect(process.env.DATABASE_URL as string)
   .then(() => console.log('Connected to MongoDB!'));
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!');
 });
