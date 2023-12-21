84d6aaf9267e39f6a1f1b045815bc3f4e8867a34
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-21 13:05:43 +0330

    feat: Implement routes and controllers

---
+++ controllers/index.ts
@@ -0,0 +1,5 @@
+const createShortenerURL = (req, res, next) => {};
+
+const getOriginalURL = (req, res, next) => {};
+
+export { createShortenerURL, getOriginalURL };
--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import app from './app';
 import mongoose from 'mongoose';
-
-const app = express();
 
 mongoose
   .connect(process.env.DATABASE_URL as string)
   .then(() => console.log('Connected to MongoDB!'));
 
+  
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log(`Server listening on port ${process.env.PORT || 3000}`);
 });
