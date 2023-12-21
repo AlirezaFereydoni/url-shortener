0643c2fa14b8ef954bec1559ec6acd160baee9dc
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-21 13:31:07 +0330

    feat: add createshortener url api

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,5 +1,27 @@
-const createShortenerURL = (req, res, next) => {};
+import { Request, Response, NextFunction } from 'express';
+import encrypt from 'js-sha256';
+import URL from '../models';
 
-const getOriginalURL = (req, res, next) => {};
+const createShortenerURL = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const originalUrl = req.body.url;
+    if (!originalUrl)
+      res.status(400).json({
+        message: 'send a valid url',
+      });
+
+    const hashedUrl = encrypt.sha256(originalUrl);
+    console.log(hashedUrl);
+
+    const shortenerUrl = await URL.create({ originalUrl: originalUrl, shortUrl: hashedUrl });
+
+    res.status(201).json({
+      message: 'Your shortened url created successfully',
+      ...shortenerUrl,
+    });
+  } catch (err) {}
+};
+
+const getOriginalURL = (req: Request, res: Response, next: NextFunction) => {};
 
 export { createShortenerURL, getOriginalURL };
--- models/index.ts
+++ models/index.ts
@@ -1,5 +1,22 @@
 import mongoose from 'mongoose';
 
-const UrlSchema = new mongoose.Schema({});
+const UrlSchema = new mongoose.Schema({
+  shortUrl: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  originalUrl: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  createdAt: {
+    default: Date.now(),
+    type: Date,
+  },
+});
 
 const URL = mongoose.model('URL', UrlSchema);
+
+export default URL;
