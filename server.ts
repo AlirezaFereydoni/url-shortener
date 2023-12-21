56437ab846f3eb57d9b10ba762a5ebe99a1f99d3
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-21 12:30:19 +0330

    feat: add config.env

--- server.ts
+++ server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(3000, () => {
+console.log(process.env);
+
+mongoose
+  .connect(process.env.DATABASE_URL as string)
+  .then(() => console.log('Connected to MongoDB!'));
+
+app.listen(process.env.PORT, () => {
   console.log('Example app listening on port 3000!');
 });
