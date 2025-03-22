fd5392e53f77220d908408be6890443b5cd8aa5a
Alireza Fereydoni <Aliirzw@gmail.com>
2025-03-22 15:12:44 +0330

    feat: add cors and helmet middleware

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
-import express from 'express';
-import Routes from './routes';
+import express from "express";
+import Routes from "./routes";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/', Routes);
+app.use(helmet());
+app.use(cors());
+
+app.use("/", Routes);
 
 export default app;
